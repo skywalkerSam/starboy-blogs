63629d1007d111708f220bef7e1a7e205257a91e
Starboy <samskywalker001@gmail.com>
2025-08-04 17:54:04 +0000

    initials

---
+++ src/app/_components/stardate-formatter.tsx
@@ -0,0 +1,15 @@
+import { parseISO, format } from "date-fns";
+
+type Props = {
+  dateString: string;
+};
+
+const StardateFormatter = ({ dateString }: Props) => {
+  const date = parseISO(dateString);
+  const formatter = format(date, "LLLL	d, yyyy");
+  const stardate = formatter.get
+  console.log(stardate);
+  return <time dateTime={dateString}>{stardate}</time>;
+};
+
+export default StardateFormatter;
