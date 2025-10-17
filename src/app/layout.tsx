e469d069f0b2095403b43b7f5567cb7f5928cd06
Starboy <samskywalker001@gmail.com>
2025-10-17 10:30:26 +0000

    light theme by default

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,23 +2,10 @@ import "~/styles/globals.css";
 import type { Metadata } from "next";
 import { ThemeProvider } from "~/components/ui/theme-provider";
 import Footer from "~/components/Footer";
-// import { Ubuntu } from "next/font/google";
-// import { Geist } from "next/font/google";
 import { Paprika } from "next/font/google";
 import { HOME_OG_IMAGE_URL } from "lib/constants";
 // import { CMS_NAME} from "lib/constants";
 
-// const ubuntu = Ubuntu({
-//   weight: ["400", "700"],
-//   style: ["normal", "italic"],
-//   subsets: ["latin"],
-//   display: "swap",
-// });
-
-// const geist = Geist({
-//   subsets: ["latin"],
-//   variable: "--font-geist-sans",
-// });
 
 const paprika = Paprika({
   subsets: ["latin"],
@@ -49,7 +36,7 @@ export default function RootLayout({
         <ThemeProvider
           attribute="class"
           defaultTheme="light"
-          enableSystem
+          // enableSystem
           disableTransitionOnChange
         >
           <main className="bg-gradient-to-b from-transparent via-blue-400 to-transparent">
